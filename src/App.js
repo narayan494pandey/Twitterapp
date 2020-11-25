5818e0be7a894ecc9b455f4f6258fbe17ce51c1e
Narayan Pandey <npandey722@gmail.com>
2020-11-25 14:38:45 +0530

    stylesidebar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="app">
-      <h1>Hello Twitter</h1>
       <Sidebar />
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,25 +3,25 @@ import './Sidebar.css'
 import TwitterIcon from '@material-ui/icons/Twitter';
 import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
-import MessageIcon from '@material-ui/icons/Message';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import ListIcon from '@material-ui/icons/List';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import MoreIcon from '@material-ui/icons/More';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 function Sidebar() {
     return (
         <div className="sidebar">
             <TwitterIcon />
-            <SidebarOption text="Home" Icon={HomeIcon} />
+            <SidebarOption active text="Home" Icon={HomeIcon} />
             <SidebarOption text="Explore" Icon={ SearchIcon} />
             <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
-            <SidebarOption text="Messages" Icon={MessageIcon} />
-            <SidebarOption text="Bookmarks" Icon={BookmarkIcon} />
-            <SidebarOption text="Lists" Icon={ListIcon} />
-            <SidebarOption text="Profile" Icon={AccountBoxIcon} />
-            <SidebarOption text="More" Icon={MoreIcon} />
+            <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+            <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+            <SidebarOption text="Lists" Icon={ListAltIcon} />
+            <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+            <SidebarOption text="More" Icon={MoreHorizIcon} />
         
         </div>
     )
