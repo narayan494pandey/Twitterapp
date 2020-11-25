338a97e12f4a3639bacab083c891c0cb6334cdc6
Narayan Pandey <npandey722@gmail.com>
2020-11-25 13:15:00 +0530

    sidebar option

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Sidebar.css'
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import MessageIcon from '@material-ui/icons/Message';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import ListIcon from '@material-ui/icons/List';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import MoreIcon from '@material-ui/icons/More';
 function Sidebar() {
     return (
-        <div>
-            I am sidebar
+        <div className="sidebar">
+            <TwitterIcon />
+            <SidebarOption text="Home" Icon={HomeIcon} />
+            <SidebarOption text="Explore" Icon={ SearchIcon} />
+            <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+            <SidebarOption text="Messages" Icon={MessageIcon} />
+            <SidebarOption text="Bookmarks" Icon={BookmarkIcon} />
+            <SidebarOption text="Lists" Icon={ListIcon} />
+            <SidebarOption text="Profile" Icon={AccountBoxIcon} />
+            <SidebarOption text="More" Icon={MoreIcon} />
+        
         </div>
     )
 }
---
+++ src/components/SidebarOption.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SidebarOption.css'
+
+
+
+
+function SidebarOption({text,Icon}) {
+    return (
+        <div className="sidebarOption">
+         <Icon />
+         <h2>{text}</h2>
+            
+        </div>
+    )
+}
+
+export default SidebarOption
