a15c3723af3c961743639baf681116291ea15884
Narayan Pandey <npandey722@gmail.com>
2020-11-26 12:12:46 +0530

    Feed component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
-
+import Feed from './components/Feed';
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
     </div>
   );
 }
