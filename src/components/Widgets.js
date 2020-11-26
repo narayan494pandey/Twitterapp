8dab3d1b55f20a33ef8b1aab6db7a12df0641dc4
Narayan Pandey <npandey722@gmail.com>
2020-11-26 21:56:57 +0530

    widgets before styling

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Widgets.css'
+import 
+{   TwitterTimelineEmbed, 
+    TwitterShareButton, 
+    TwitterTweetEmbed,
+ } from 'react-twitter-embed';
+import { Search } from '@material-ui/icons';
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <Search  className="widgets__searchIcon"/>
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>Whats happening</h2>
+                <TwitterTweetEmbed tweetId={"1331947670550810624"} />
+                <TwitterTimelineEmbed 
+                    sourceType="profile"
+                    screenName="narendramodi"
+                    options={{height:400}}
+                />
+                <TwitterShareButton
+                    url={'https://www.facebook.com/narayan.pandey.35728/'}
+                    options={{ text: '#reactjs is awesome', via: 'Narayan27928891' }}
+              />
+            </div>
         </div>
     )
 }
