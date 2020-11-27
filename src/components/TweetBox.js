6a132b4f424a137d82b141f290ec30b2f5966afb
Narayan Pandey <npandey722@gmail.com>
2020-11-27 15:26:34 +0530

    realtime tweet

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Avatar, Button } from '@material-ui/core';
 import './TweetBox.css'
+import db from './firebase';
 
 function TweetBox() {
+    const [tweetMessage, setTweetMessage] = useState("");
+    const [tweetImage, setTweetImage] = useState(""); 
+    const sendTweet = e =>{
+        e.preventDefault();
+        db.collection('posts').add({
+            dispalyName:'Narendra Modi',
+            username:'@narendramodi',
+            verified:true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar:"https://upload.wikimedia.org/wikipedia/commons/2/2e/Prime_Minister%2C_Shri_Narendra_Modi%2C_in_New_Delhi_on_August_08%2C_2019_%28cropped%29.jpg",
+        })
+
+    }
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="/images/Narayan.jpg" />
-                    <input placeholder="What's happening?" type="text" />
+                    <input onChange={e => setTweetMessage(e.target.value)}
+                         value={tweetMessage} 
+                         placeholder="What's happening?"
+                          type="text" />
                 </div>
-                <input className="tweetBox__imageInput" placeholder="Optional Enter image URL" type="text" />
-                <Button variant="outlined" className="tweetBox__tweet">Tweet</Button>
+                <input onChange={e => setTweetImage(e.target.value)}
+                    value={tweetImage} 
+                    className="tweetBox__imageInput" 
+                    placeholder="Optional Enter image URL" 
+                    type="text" />
+                <Button onClick={sendTweet} type="submit" variant="outlined" className="tweetBox__tweet">Tweet</Button>
             </form>
         </div>
     )
